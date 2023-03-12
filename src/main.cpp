6c8fe0a7d98fed3d3fc7e57b0f55059116d48fc6
ブラッドウイング <bloodwiing@outlook.com>
2023-03-13 00:48:27 +0200

    Basic window

---
+++ src/main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include "glad/glad.h"
+#include <glfw3.h>
+
+int main() {
+    glfwInit();
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    GLFWwindow *window = glfwCreateWindow(500, 500, "Testing", NULL, NULL);
+    if (window == nullptr) {
+        std::cout << "Failed to create window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+
+    gladLoadGL();
+
+    glViewport(0, 0, 500, 500);
+
+    glClearColor(0.07f, 0.05f, 0.21f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    glfwSwapBuffers(window);
+
+    while (!glfwWindowShouldClose(window)) {
+        glfwPollEvents();
+    }
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
+    return 0;
+}
