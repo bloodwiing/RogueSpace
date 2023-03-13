aa4e7bfd9f36542735118cb0eeb7a6036bf266ad
bloodwiing <bloodwiing@outlook.com>
2023-03-13 12:36:54 +0200

    Index buffer

--- src/main.cpp
+++ src/main.cpp
@@ -53,10 +53,16 @@ int main() {
     float vertices[] = {
             -0.5, -0.5, 0.0,
             0.5, -0.5, 0.0,
-            0.0, 0.5, 0.0
+            0.5, 0.5, 0.0,
+            -0.5, 0.5, 0.0
     };
 
-    GLuint vaoID, vboID;
+    unsigned int indices[] = {
+            0, 1, 2,
+            0, 2, 3
+    };
+
+    GLuint vaoID, vboID, eboID;
 
     glGenBuffers(1, &vboID);
     glBindBuffer(GL_ARRAY_BUFFER, vboID);
@@ -67,13 +73,18 @@ int main() {
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), nullptr);
     glEnableVertexAttribArray(0);
 
+    glGenBuffers(1, &eboID);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, eboID);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
     while (!glfwWindowShouldClose(window)) {
 
         glClearColor(0.07f, 0.05f, 0.21f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
         glUseProgram(progID);
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr);
 
         glfwSwapBuffers(window);
 
