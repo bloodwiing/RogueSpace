efe2434ce8bc88a2a72da3a50c7536fcae5c7396
ブラッドウイング <bloodwiing@outlook.com>
2023-03-13 12:13:41 +0200

    Testing OpenGL

--- src/main.cpp
+++ src/main.cpp
@@ -21,12 +21,58 @@ int main() {
 
     glViewport(0, 0, 500, 500);
 
-    glClearColor(0.07f, 0.05f, 0.21f, 1.0f);
-    glClear(GL_COLOR_BUFFER_BIT);
+    const char *vertexShaderSource = ""
+        "#version 330 core\n"
+        "layout (location = 0) in vec3 pos;\n"
+        "void main() {\n"
+        "  gl_Position = vec4(pos, 1.0);\n"
+        "}\0";
+    const char *fragmentShaderSource = ""
+         "#version 330 core\n"
+         "layout (location = 0) in vec3 pos;\n"
+         "void main() {\n"
+         "  gl_Position = vec4(pos, 1.0);\n"
+         "}\0";
 
-    glfwSwapBuffers(window);
+    GLuint vertID = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertID, 1, &vertexShaderSource, nullptr);
+    glCompileShader(vertID);
+
+    GLuint fragID = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragID, 1, &fragmentShaderSource, nullptr);
+    glCompileShader(fragID);
+
+    GLuint progID = glCreateProgram();
+    glAttachShader(progID, vertID);
+    glAttachShader(progID, fragID);
+    glLinkProgram(progID);
+
+    glDeleteShader(vertID);
+    glDeleteShader(fragID);
+
+    float vertices[] = {
+            -1.0, -1.0, 0.0,
+            0.0, -1.0, 0.0,
+            0.0, 0.0, 0.0
+    };
+
+    GLuint vaoID, vboID;
+
+    glGenBuffers(1, &vboID);
+    glBindBuffer(GL_ARRAY_BUFFER, vboID);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glGenVertexArrays(1, &vaoID);
+    glVertexAttribPointer(vaoID, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), nullptr);
+    glEnableVertexAttribArray(vaoID)  // todo: here
 
     while (!glfwWindowShouldClose(window)) {
+
+        glClearColor(0.07f, 0.05f, 0.21f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glfwSwapBuffers(window);
+
         glfwPollEvents();
     }
 
