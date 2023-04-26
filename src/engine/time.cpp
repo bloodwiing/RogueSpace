4b55d49dc70613ad56b389fac34238e1da805d12
ブラッドウイング <bloodwiing@outlook.com>
2023-04-26 23:11:47 +0300

    Refactored Time module, better sleep till next frame

--- src/engine/time.cpp
+++ src/engine/time.cpp
@@ -3,20 +3,21 @@
 #include <chrono>
 #include <thread>
 
-std::chrono::time_point<std::chrono::steady_clock> Time::m_lastTime = std::chrono::steady_clock::now();
-std::chrono::time_point<std::chrono::steady_clock> Time::m_frameStartTime = Time::m_lastTime;
-std::chrono::duration<double, std::ratio<1, 1>> Time::m_delta = std::chrono::duration<double, std::ratio<1, 1>>(0.0);
+TTimePoint Time::m_frameStart = std::chrono::steady_clock::now();
+TTimePoint Time::m_frameEnd = Time::m_frameStart;
+TDoubleSec Time::m_delta = TDoubleSec(0.0);
 std::chrono::microseconds Time::m_requiredFrameTime(0);
 
 void Time::init() {
-    m_lastTime = std::chrono::steady_clock::now();
+    m_frameStart = std::chrono::steady_clock::now();
+    update();
 }
 
 void Time::update() {
     auto newTime = std::chrono::steady_clock::now();
-    m_delta = newTime - m_lastTime;
-    m_frameStartTime = m_lastTime;
-    m_lastTime = newTime;
+    m_delta = newTime - m_frameStart;
+    m_frameStart = newTime;
+    m_frameEnd = m_frameStart + m_requiredFrameTime;
 }
 
 void Time::setMaxFramerate(int framerate) {
@@ -24,6 +25,10 @@ void Time::setMaxFramerate(int framerate) {
         m_requiredFrameTime = std::chrono::microseconds(0);
     else
         m_requiredFrameTime = std::chrono::microseconds(1'000'000 / framerate);
+}
+
+std::chrono::microseconds Time::getMinimumFrameTime() {
+    return m_requiredFrameTime;
 }
 
 float Time::getDeltaFloat() {
@@ -34,8 +39,11 @@ double Time::getDeltaDouble() {
     return m_delta.count();
 }
 
+void Time::setDelta(TDoubleSec delta) {
+    m_delta = delta;
+}
+
 void Time::waitForNextFrame() {
-    if (m_requiredFrameTime.count() != 0) {
-        std::this_thread::sleep_until(m_frameStartTime + m_requiredFrameTime);
-    }
+    std::this_thread::sleep_until(m_frameEnd - std::chrono::milliseconds(50));
+    while (std::chrono::steady_clock::now() < m_frameEnd);
 }
