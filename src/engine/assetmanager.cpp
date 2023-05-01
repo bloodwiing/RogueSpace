4c1e5a00740433a513c54ff19e30b5291736bc9a
ブラッドウイング <bloodwiing@outlook.com>
2023-05-02 00:43:37 +0300

    Asset Manager

---
+++ src/engine/assetmanager.cpp
@@ -0,0 +1,48 @@
+#include "engine/assetmanager.hpp"
+
+Engine::AssetManager* Engine::AssetManager::m_instance = nullptr;
+
+Engine::AssetManager::AssetManager()
+    : m_models()
+    , m_textures()
+    , m_modelMutex()
+    , m_textureMutex()
+{
+
+}
+
+Engine::AssetManager *Engine::AssetManager::getInstance() {
+    if (m_instance == nullptr)
+        m_instance = new AssetManager();
+    return m_instance;
+}
+
+std::shared_ptr<Graphics::Model> Engine::AssetManager::getModel(const std::string &fileName) {
+    std::lock_guard<std::mutex> lock(m_modelMutex);
+    if (m_models.find(fileName) == m_models.end()) {
+        auto model = loadModel(fileName);
+        m_models[fileName] = model;
+    }
+    return m_models.at(fileName);
+}
+
+std::shared_ptr<Graphics::Texture> Engine::AssetManager::getTexture(const std::string &fileName) {
+    std::lock_guard<std::mutex> lock(m_textureMutex);
+    if (m_textures.find(fileName) == m_textures.end()) {
+        auto texture = loadTexture(fileName);
+        m_textures[fileName] = texture;
+    }
+    return m_textures.at(fileName);
+}
+
+std::shared_ptr<Graphics::Model> Engine::AssetManager::loadModel(const std::string &fileName) {
+    auto model = Graphics::Model::create(fileName);
+    model->queue();
+    return model;
+}
+
+std::shared_ptr<Graphics::Texture> Engine::AssetManager::loadTexture(const std::string &fileName) {
+    auto texture = Graphics::Texture::create(fileName);
+    texture->queue();
+    return texture;
+}
