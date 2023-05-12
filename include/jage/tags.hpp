29fd705e51cc2a208db4f0cadcfea06218b96e30
ブラッドウイング <bloodwiing@outlook.com>
2023-05-12 16:40:15 +0300

    Enum Size (Tags)

--- include/jage/tags.hpp
+++ include/jage/tags.hpp
@@ -1,6 +1,8 @@
 #ifndef JAGE_TAGS_HPP
 #define JAGE_TAGS_HPP
 
+#include "jage/utility/utility.hpp"
+
 namespace jage {
 
     enum Tag {
@@ -18,5 +20,10 @@ namespace jage {
         BULLET,
     };
 }
+
+template<>
+struct jage::utility::EnumSize<jage::Tag> {
+    static const size_t value = Tag::BULLET + 1;
+};
 
 #endif //JAGE_TAGS_HPP
