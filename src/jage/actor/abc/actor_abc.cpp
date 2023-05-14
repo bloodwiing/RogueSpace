403d4a2f8ca17ab97a46669b02d555840c5a5087
ブラッドウイング <bloodwiing@outlook.com>
2023-05-14 22:40:17 +0300

    Fix Scene-less Actors

--- src/jage/actor/abc/actor_abc.cpp
+++ src/jage/actor/abc/actor_abc.cpp
@@ -20,7 +20,8 @@ ActorABC::ActorABC(ActorABC *parent, std::string& name, Tag tag, bool isVolatile
 }
 
 void ActorABC::tagToScene(Scene *scene) {
-    scene->tagActorToMap(shared_from_this());
+    if (scene != nullptr)
+        scene->tagActorToMap(shared_from_this());
 }
 
 std::map<std::string, ActorABC::ChildEntry> ActorABC::getChildren() const {
