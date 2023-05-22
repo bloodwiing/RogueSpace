7341c1f6ecafdb8315fb007c4764cbf8b0c88c8a
bloodwiing <bloodwiing@outlook.com>
2023-05-22 12:11:18 +0300

    RectNodeABC

--- include/jage/node/frame/abc/frame_abc.hpp
+++ include/jage/node/frame/abc/frame_abc.hpp
@@ -2,10 +2,13 @@
 #define JAGE_CANVAS_ABC_HPP
 
 #include "jage/node/abc/node_abc.hpp"
+#include "jage/node/abc/transformable2d_abc.hpp"
+#include "jage/node/abc/rectnode_abc.hpp"
 #include "jage/type/rect.hpp"
 #include "jage/node/base/dying_base.hpp"
+#include "jage/script/abc/scriptable_abc.hpp"
 
-#define JAGE_FRAME_ARGS JAGE_NODE_ARGS(jage::node::frame::abc::FrameABC), jage::node::Canvas* canvas
+#define JAGE_FRAME_ARGS JAGE_NODE_ARGS(jage::node::frame::abc::FrameABC), jage::node::Canvas* canvas, jage::node::abc::RectNodeABC* rectParent
 
 namespace jage::node {
     class Canvas;
@@ -13,17 +16,25 @@ namespace jage::node {
 
 namespace jage::node::frame::abc {
 
-    class FrameABC : public node::abc::NodeABC<FrameABC>, public jage::node::base::DyingBase {
+    class FrameABC
+            : public node::abc::NodeABC<FrameABC>
+            , public node::abc::RectNodeABC
+            , public jage::node::abc::Transformable2DABC
+            , public script::abc::ScriptableABC
+            , public jage::node::base::DyingBase {
     public:
         FrameABC(JAGE_FRAME_ARGS);
 
-        [[nodiscard]] virtual jage::type::RectF getRect() const = 0;
-        [[nodiscard]] virtual jage::type::RectF getAnchor() const = 0;
-        [[nodiscard]] virtual jage::type::RectF getPhysicalRect() const = 0;
+        [[nodiscard]] std::string getTypeName() const override;
 
     private:
         Canvas* m_canvas;
+        RectNodeABC* m_rectParent;
+
+        using node::abc::NodeABC<FrameABC>::update;
     };
 }
+
+#include "frame_abc_impl.tpp"
 
 #endif //JAGE_CANVAS_ABC_HPP
--- src/jage/node/frame/abc/frame_abc.cpp
+++ src/jage/node/frame/abc/frame_abc.cpp
@@ -5,6 +5,37 @@ using jage::node::frame::abc::FrameABC;
 FrameABC::FrameABC(JAGE_FRAME_ARGS)
     : node::abc::NodeABC<FrameABC>(parent, std::move(name))
     , m_canvas(canvas)
+    , m_rectParent(rectParent)
 {
 
+}
+
+jage::node::Canvas* FrameABC::getCanvas() const {
+    return m_canvas;
+}
+
+jage::node::abc::RectNodeABC* FrameABC::getRectParent() const {
+    return m_rectParent;
+}
+
+void FrameABC::update() {
+    DyingBase::updateDeathTimer();
+    script::abc::ScriptableABC::update();
+    NodeABC::update();
+}
+
+void FrameABC::kill() {
+    DyingBase::kill();
+}
+
+void FrameABC::kill(float delay) {
+    DyingBase::kill(delay);
+}
+
+bool FrameABC::isDead() const {
+    return DyingBase::isDead();
+}
+
+std::string FrameABC::getTypeName() const {
+    return "Frame ABSTRACT BASE CLASS";
 }
