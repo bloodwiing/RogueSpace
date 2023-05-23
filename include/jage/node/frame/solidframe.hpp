4ee3d9955f7ea1cbe6c3c56cb1e19dc2bbb9fe76
ブラッドウイング <bloodwiing@outlook.com>
2023-05-24 01:19:15 +0300

    SolidFrame

--- include/jage/node/frame/solidframe.hpp
+++ include/jage/node/frame/solidframe.hpp
@@ -2,6 +2,8 @@
 #define JAGE_SOLID_FRAME_HPP
 
 #include "jage/node/frame/basicframe.hpp"
+#include "jage/graphics/mesh2d/sprite.hpp"
+#include "jage/graphics/shader.hpp"
 
 namespace jage::node::frame {
 
@@ -9,7 +11,21 @@ namespace jage::node::frame {
             : public BasicFrame
     {
     public:
+        SolidFrame(JAGE_FRAME_ARGS, std::shared_ptr<jage::graphics::Shader> shader);
+
         void draw() override;
+
+        virtual void setSprite(std::shared_ptr<jage::graphics::mesh2d::Sprite> sprite);
+
+        virtual std::shared_ptr<jage::graphics::mesh2d::Sprite> getSprite() const;
+
+    protected:
+        void updateReflow() override;
+
+    private:
+        graphics::mesh2d::Quad2D m_quad;
+        std::shared_ptr<jage::graphics::mesh2d::Sprite> m_sprite;
+        std::shared_ptr<jage::graphics::Shader> m_shader;
     };
 }
 
--- src/jage/node/frame/solidframe.cpp
+++ src/jage/node/frame/solidframe.cpp
@@ -1,5 +1,37 @@
+#include <utility>
+
 #include "jage/node/frame/solidframe.hpp"
 
-void jage::node::frame::SolidFrame::draw() {
+using jage::node::frame::SolidFrame;
+using jage::type::RectF;
+using jage::graphics::mesh2d::Sprite;
+using jage::graphics::mesh2d::Quad2D;
+using jage::graphics::Shader;
+
+SolidFrame::SolidFrame(JAGE_FRAME_ARGS, std::shared_ptr<Shader> shader)
+        : BasicFrame(parent, std::move(name), canvas, rectParent, rect, anchor)
+        , m_sprite(Sprite::getDefaultSprite())
+        , m_quad(RectF(1.0f, 1.0f), Sprite::getDefaultSprite())
+        , m_shader(std::move(shader))
+{
+
+}
+
+void SolidFrame::draw() {
+    m_quad.draw(*m_shader, BasicFrame::getWorldMatrix());
     BasicFrame::draw();
+}
+
+void SolidFrame::updateReflow() {
+    FrameABC::updateReflow();
+    m_quad = Quad2D(getPhysicalRect(), getSprite());
+}
+
+void SolidFrame::setSprite(std::shared_ptr<Sprite> sprite) {
+    m_sprite = std::move(sprite);
+    m_quad = Quad2D(getPhysicalRect(), getSprite());
+}
+
+std::shared_ptr<Sprite> SolidFrame::getSprite() const {
+    return m_sprite;
 }
