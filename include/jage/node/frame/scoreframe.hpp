0eea081480b8e62077bb3e7a72b9ce0cfc40ee3e
ブラッドウイング <bloodwiing@outlook.com>
2023-05-28 23:55:26 +0300

    Score Frame (Score Counter UI)

---
+++ include/jage/node/frame/scoreframe.hpp
@@ -0,0 +1,38 @@
+#ifndef JAGE_SCORE_FRAME_HPP
+#define JAGE_SCORE_FRAME_HPP
+
+#include "jage/node/frame/basicframe.hpp"
+#include "jage/node/frame/spriteframe.hpp"
+
+namespace jage::node::frame {
+
+    class ScoreFrame
+            : public BasicFrame
+    {
+    public:
+        ScoreFrame(JAGE_FRAME_ARGS, std::string digitFilePath, int minSize = 0);
+
+        void setScore(long score);
+
+        [[nodiscard]] long getScore() const;
+
+        void update() override;
+
+    private:
+        std::string m_digitFilePath;
+
+        std::vector<std::shared_ptr<graphics::mesh2d::Sprite>> m_digits;
+
+        std::vector<SpriteFrame*> m_scoreFrames;
+
+        int getSizeOfNumber(long number);
+
+        bool m_outdatedScore = false;
+
+        long m_score;
+        int m_size = 0;
+        const int m_minSize;
+    };
+}
+
+#endif //JAGE_SCORE_FRAME_HPP
--- src/game/canvas/hudcanvas.cpp
+++ src/game/canvas/hudcanvas.cpp
@@ -2,6 +2,7 @@
 
 #include "jage/node/frame/anchor.hpp"
 #include "jage/node/frame/spriteframe.hpp"
+#include "jage/node/frame/scoreframe.hpp"
 
 using game::canvas::HUDCanvas;
 using jage::node::Canvas;
@@ -13,7 +14,14 @@ using namespace jage::node::frame;
 std::unique_ptr<Canvas> HUDCanvas::create() {
     auto canvas = std::make_unique<Canvas>(RectI32(1920, 1080));
 
-    canvas->addChild<SpriteFrame>("Flash", RectI32(1920, 1080), Anchor::Full, "./res/sprite/flash.sprite");
+    auto flash = canvas->addChild<SpriteFrame>("Flash", RectI32(1920, 1080), Anchor::Full, "./res/sprite/flash.sprite");
+    flash->setMultiply(0.835f, 0.082f, 0.416f, 0.2f);
+
+    canvas->addChild<SpriteFrame>("HealthBar", RectI32(50, 50, 380, 106), Anchor::BottomLeft, "./res/sprite/hud/BarFull.sprite");
+    canvas->addChild<SpriteFrame>("IconWarning", RectI32(405, 50, 461, 106), Anchor::BottomLeft, "./res/sprite/hud/IconWarning.sprite");
+
+    auto score = canvas->addChild<ScoreFrame>("Score", RectI32(1, 1), Anchor::BottomRight, "./res/sprite/hud/Digit", 4);
+    score->setScore(1);
 
     return canvas;
 }
