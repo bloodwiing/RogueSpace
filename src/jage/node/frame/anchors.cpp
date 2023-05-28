54eb3a2ed184f3ad99d479e4e440e43705747e5c
ブラッドウイング <bloodwiing@outlook.com>
2023-05-28 22:15:17 +0300

    Fast Anchors (RectF pre-definitions)

---
+++ src/jage/node/frame/anchors.cpp
@@ -0,0 +1,26 @@
+#include "jage/node/frame/anchors.hpp"
+
+using jage::node::frame::Anchor;
+using jage::type::RectF;
+
+const RectF Anchor::TopLeft = RectF(1.0f, 0.0f, 1.0f, 0.0f);
+const RectF Anchor::TopCenter = RectF(1.0f, 0.5f, 1.0f, 0.5f);
+const RectF Anchor::TopRight = RectF(1.0f, 1.0f, 1.0f, 1.0f);
+
+const RectF Anchor::MiddleLeft = RectF(0.5f, 0.0f, 0.5f, 0.0f);
+const RectF Anchor::MiddleCenter = RectF(0.5f, 0.5f, 0.5f, 0.5f);
+const RectF Anchor::MiddleRight = RectF(0.5f, 1.0f, 0.5f, 1.0f);
+
+const RectF Anchor::BottomLeft = RectF(0.0f, 0.0f, 0.0f, 0.0f);
+const RectF Anchor::BottomCenter = RectF(0.5f, 0.0f, 0.5f, 0.0f);
+const RectF Anchor::BottomRight = RectF(1.0f, 0.0f, 1.0f, 0.0f);
+
+const RectF Anchor::Top = RectF(0.0f, 1.0f, 1.0f, 1.0f);
+const RectF Anchor::Middle = RectF(0.0f, 0.5f, 1.0f, 0.5f);
+const RectF Anchor::Bottom = RectF(0.0f, 0.0f, 1.0f, 0.0f);
+
+const RectF Anchor::Left = RectF(0.0f, 0.0f, 0.0f, 1.0f);
+const RectF Anchor::Center = RectF(0.5f, 0.0f, 0.5f, 1.0f);
+const RectF Anchor::Right = RectF(1.0f, 0.0f, 1.0f, 1.0f);
+
+const RectF Anchor::Full = RectF(0.0f, 0.0f, 1.0f, 1.0f);
