9d418470d2561b0d9215492bfc9c073c78fcafb7
ブラッドウイング <bloodwiing@outlook.com>
2023-05-28 21:55:43 +0300

    All-in-one Actor header

---
+++ include/jage/node/actor/actors.hpp
@@ -0,0 +1,11 @@
+#ifndef JAGE_ACTORS_HPP
+#define JAGE_ACTORS_HPP
+
+#include "./camera.hpp"
+#include "./dynamicactor.hpp"
+#include "./modelactor.hpp"
+#include "./physicsactor.hpp"
+#include "./shipactor.hpp"
+#include "./staticactor.hpp"
+
+#endif //JAGE_ACTORS_HPP
